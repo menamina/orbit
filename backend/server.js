a0b59f6e018154c99f50faf9d9b1e4cf6a1efe4b
Mena <dayjacs03@gmail.com>
2026-06-18 22:11:18 -0500

    setting up backend

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,15 @@ import { express } from "express";
 const server = express();
 const port = process.env.PORT;
 const router = require("./routes");
+
+function handleError(error) {
+  if (error) {
+    console.error("WHOMP :-(", error);
+    return;
+  }
+  console.log(`yay :-)`);
+}
+
+server.listen(port, handleError);
+
+module.exports = server;
