46e9b7e2e5a1487f2563af79ae87c05afc46bedd
Mena <dayjacs03@gmail.com>
2026-07-23 16:48:18 -0500

    routes

---
+++ backend/router/routes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/");
+router.get("/api/signup");
+router.get("/api/login");
+router.get("/api/logout");
+
+router.get("/api/month/:month/:year");
+
+// ======== PILL ======== \\
+router.post("/api/track/pill");
+router.delete("/api/track/pill/delete/:pillID");
+
+// ======== CYCLEs ======== \\
+router.post("/api/track/period");
+router.delete("/api/track/pill/delete/:pillID");
+
+// ======== SETTINGS ======== \\
+router.get("/api/settings");
+router.patch("/api/update/icon");
+router.patch("/api/update/color");
+router.patch("/api/update/password");
+router.patch("/api/update/email");
+
+// ======== DLT ACC ======== \\
+router.delete("/api/delete/account");
+
+module.exports = router;
