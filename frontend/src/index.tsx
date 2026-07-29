ba27da641c80eb6546aaab6126f83826f6e74e4a
Mena <dayjacs03@gmail.com>
2026-07-29 11:53:29 -0500

    working on frontend jwt int

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,1 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [accessToken, setAccessToken] = useState();
+
+  const { data: tokenData, error: tokenError } = useQuery(authenticate());
+}
+
+export default App;
