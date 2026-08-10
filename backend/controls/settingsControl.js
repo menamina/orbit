0ec17184194bd8ccfa97a56eea41996bf40fea6a
Mena <dayjacs03@gmail.com>
2026-08-10 15:11:06 -0500

    user settings controller

--- backend/controls/settingsControl.js
+++ backend/controls/settingsControl.js
@@ -1,4 +1,5 @@
 import prisma from "../prisma/client";
+import { passwordGenie, checkPassword } from "../utils/passwordUtil";
 
 async function settingsUpdate(req, res){
     try {
@@ -6,7 +7,27 @@ async function settingsUpdate(req, res){
         const userID = Number(req.user.userID)
         const { name, username, email } = req.body
 
+        if (name === "" && username === "" && email === ""){
+            return res.status(400).json({error: "Cannot update with empty values"})
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: { id: userID },
+            data: {
+                ...(name && {name}),
+                ...(username && {username}),
+                ...(email && {email})
+            }
+        })
+
+
+        // check user + email in front end
+
+        return res.status(200).json({success: true})
+
     } catch(error){
+        console.log(error);
+        return res.status(500).json({ serverError: "Server error" });
 
     }
 }
@@ -15,7 +36,27 @@ async function settingsUpdate(req, res){
 async function changePassword(req, res){
     try {
 
+        const userID = Number(req.user.userID)
+        const { oldPassword, password } = req.body
 
+        const user = await prisma.settings.findUnique({
+            where: { userId: userID }
+        });
+
+        const match = checkPassword(oldPassword, user.saltedHash)
+
+            if (!isValid) {
+      return res.status(400).json({ error: "Current password is incorrect" });
+    }
+
+        const newPassword = passwordGenie(password)
+
+        await prisma.settings.update({
+      where: { userId: userID },
+      data: { saltedHash: newPassword }
+    });
+
+       return res.status(200).json({success: true})
 
     } catch(error){
         console.log(error);
