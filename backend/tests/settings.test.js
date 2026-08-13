b23b0bc5d18c53593e53452cb6536ba33b292724
Mena <dayjacs03@gmail.com>
2026-08-13 15:19:51 -0500

    settings test

---
+++ backend/tests/settings.test.js
@@ -0,0 +1,69 @@
+import app from "../../server";
+import supertest from "supertest";
+const request = supertest(app);
+const agent = supertest.agent(app);
+import prisma from "../../prisma/client";
+import { passwordGenie } from "../../utils/password";
+
+jest.setTimeout(5500);
+
+let user;
+let pill;
+
+async function createTestUser(
+  username = "orbiter",
+  email = "test@gmail.com",
+  password = "777IMINHEAVEN",
+) {
+  const hashedPassword = await passwordGenie(password);
+  return await prisma.user.create({
+    data: {
+      name: "Orbit",
+      username,
+      email,
+      settings: {
+        create: {
+          saltedHash: hashedPassword,
+        },
+      },
+      accounts: {
+        create: {
+          provider: "local",
+          providerId: email,
+        },
+      },
+    },
+  });
+}
+
+async function login() {
+  return await agent.post("/api/login").send({
+    email: "test@gmail.com",
+    password: "777IMINHEAVEN",
+  });
+}
+
+async function logout() {
+  await agent.post("/api/logout");
+}
+
+async function dlt(userID) {
+  await prisma.user.delete({ where: { id: userID } });
+}
+
+async function dltAll() {
+  await prisma.user.deleteMany({});
+}
+
+beforeAll(async () => {
+  await dltAll();
+  user = await createTestUser();
+  await login();
+});
+
+afterAll(async () => {
+  if (user) {
+    await dlt(user.id);
+  }
+  await prisma.$disconnect();
+});
