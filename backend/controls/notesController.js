33e95d91368f7174d08dd71c22f7cd91792b8043
Mena <dayjacs03@gmail.com>
2026-08-14 18:49:06 -0500

    notes

--- backend/controls/notesController.js
+++ backend/controls/notesController.js
@@ -3,6 +3,26 @@ import prisma from "../../prisma/client";
 async function getNotes(req, res) {
   try {
     const userID = Number(req.user.userID);
+    const month = Number(req.body.month);
+    const year = Number(req.body.year);
+
+    const startOfMonth = new Date(yearNum, monthNum - 1, 1);
+    const startOfNextMonth = new Date(yearNum, monthNum, 1);
+
+    const usersNotes = await prisma.notes.findMany({
+      where: {
+        userID,
+        date: {
+          gte: startOfMonth,
+          lt: startOfMonth,
+        },
+      },
+      orderBy: {
+        date: "asc",
+      },
+    });
+
+    return res.status(200).json(usersNotes);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ serverError: "Server error" });
@@ -12,6 +32,18 @@ async function getNotes(req, res) {
 async function writeNote(req, res) {
   try {
     const userID = Number(req.user.userID);
+    const note = req.body.note;
+
+    const newNote = await prisma.notes.create({
+      where: {
+        userID,
+      },
+      data: {
+        note,
+      },
+    });
+
+    return res.status(200).json(newNote);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ serverError: "Server error" });
