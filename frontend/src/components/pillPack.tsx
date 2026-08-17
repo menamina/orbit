c9b1aa718e6526fb538718ea26d906fe9cd78205
Mena <dayjacs03@gmail.com>
2026-08-17 14:00:50 -0500

    changing how pill component is structured for start of week selection

--- frontend/src/components/pillPack.tsx
+++ frontend/src/components/pillPack.tsx
@@ -1,26 +1,25 @@
-import { useState } from "react";
-
-import type { PillPack, Pill } from "./pillTypes";
+import type { PillPack as PillPackType, Pill } from "../tanstack/pillTypes";
 
 import { Box, Paper } from "@mui/material";
 
-function PillPack(currentPack: PillPack) {
-  const [dayOfTheWeekToStart, setDayOfTheWeekToStart] = useState("sun")
+interface PillPackProps {
+  currentPack?: PillPackType;
+  dayOfTheWeekToStart: string;
+}
 
-
+function PillPack({ currentPack, dayOfTheWeekToStart }: PillPackProps) {
   const days = [];
   for (let x = 0; x < 28; x++) {
     days.push(x + 1);
   }
 
   const weekdays = ["sun", "mon", "tues", "wed", "thur", "fri", "sat"];
+
   const startDayIndex = weekdays.indexOf(dayOfTheWeekToStart);
   const startPackThisDay = [
     ...weekdays.slice(startDayIndex),
     ...weekdays.slice(0, startDayIndex),
   ];
-
-  const today = new Date();
 
   return (
     <>
@@ -35,37 +34,11 @@ function PillPack(currentPack: PillPack) {
             gap: "5px",
           }}
         >
-          {currentPack?.pills?.length === 0 ? (
-            <>
-              <Paper sx={{ gridColumn: "1 / -1", textAlign: "center" }}>
-                Pick a day to start your pack
-              </Paper>
-              {weekdays.map((day) => (
-                <Paper
-                  key={day}
-                  onClick={() => setDayOfTheWeekToStart(day)}
-                  sx={{
-                    cursor: "pointer",
-                    textAlign: "center",
-                    bgcolor: dayOfTheWeekToStart === day ? "#1b1a61" : "white",
-                    color: dayOfTheWeekToStart === day ? "white" : "black",
-                    "&:hover": {
-                      bgcolor:
-                        dayOfTheWeekToStart === day ? "#1b1a61" : "#f0f0f0",
-                    },
-                  }}
-                >
-                  {day}
-                </Paper>
-              ))}
-            </>
-          ) : (
-            startPackThisDay.map((day) => (
-              <Paper key={day} sx={{ textAlign: "center" }}>
-                {day}
-              </Paper>
-            ))
-          )}
+          {startPackThisDay.map((day) => (
+            <Paper key={day} sx={{ textAlign: "center", padding: "5px" }}>
+              {day}
+            </Paper>
+          ))}
         </Box>
         <Box
           sx={{
