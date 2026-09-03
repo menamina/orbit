ddf98b7968c7669bbea6a7cbcc79deaf9ba1d4c2
Mena <dayjacs03@gmail.com>
2026-09-02 22:32:42 -0500

    notes

--- frontend/src/components/notes/writeOrEditNote.tsx
+++ frontend/src/components/notes/writeOrEditNote.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
+import { Box, Paper, Button } from "@mui/material";
+
 type NoteData = {
   id: number | null;
   note: string | null;
 };
 
 function Note({ noteData = null }: NoteData) {
+  const today = Date.now();
+
   return (
     <>
+      <Box>{today}</Box>
       {noteData === null && (
         <Box>
-          <Box>Today's date here</Box>
+          <Box></Box>
           <Box></Box>
         </Box>
       )}
