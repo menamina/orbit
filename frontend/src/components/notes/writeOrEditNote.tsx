c039648cbad1a34de5c1217b4b335b8a2696785d
Mena <dayjacs03@gmail.com>
2026-09-04 17:22:05 -0500

    note comp

--- frontend/src/components/notes/writeOrEditNote.tsx
+++ frontend/src/components/notes/writeOrEditNote.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Box, Paper, Button } from "@mui/material";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
 
 type NoteData = {
   id: number | null;
@@ -7,18 +8,32 @@ type NoteData = {
 };
 
 function Note({ noteData = null }: NoteData) {
-  const today = Date.now();
-
   return (
-    <>
-      <Box>{today}</Box>
+    <Box
+      sx={{
+        padding: "40px",
+      }}
+    >
       {noteData === null && (
-        <Box>
-          <Box></Box>
-          <Box></Box>
+        <Box
+          sx={{
+            display: "flex",
+            direction: "column",
+          }}
+        >
+          <Box>something with a thought bubble</Box>
+          <Box>
+            <TextareaAutosize
+              value={noteData?.note ? noteData?.note : null}
+              placeholder="....."
+              aria-label="write a note"
+              minRows={15}
+              style={{ width: 200 }}
+            />
+          </Box>
         </Box>
       )}
-    </>
+    </Box>
   );
 }
 
