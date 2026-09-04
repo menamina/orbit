9923ae939c6f040f0abb1aa3b19001f287c70f7f
Mena <dayjacs03@gmail.com>
2026-09-04 17:40:06 -0500

    note comp

--- frontend/src/components/notes/writeOrEditNote.tsx
+++ frontend/src/components/notes/writeOrEditNote.tsx
@@ -2,12 +2,38 @@ import { useState } from "react";
 import { Box, Paper, Button } from "@mui/material";
 import TextareaAutosize from "@mui/material/TextareaAutosize";
 
+import { writeANoteMut, updateNoteMut, dltNoteMut } from "../tanstack/notesTS";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 type NoteData = {
-  id: number | null;
-  note: string | null;
+  id: number;
+  note: string;
 };
 
-function Note({ noteData = null }: NoteData) {
+function Note({ noteData = null }: { noteData: NoteData | null }) {
+  const [note, setNote] = useState({
+    id: noteData?.id ? noteData.id : "",
+    note: noteData?.note ? noteData.note : "",
+  });
+
+  const { data: writeNote, isPending: writePending } = useMutation({
+    ...writeANoteMut(),
+    onSuccess: () => {},
+    onError: (error) => {},
+  });
+
+  const { data: updateNote, isPending: updatePending } = useMutation({
+    ...updateNoteMut(),
+    onSuccess: () => {},
+    onError: (error) => {},
+  });
+
+  const { data: dltNote, isPending: dltPending } = useMutation({
+    ...dltNoteMut(),
+    onSuccess: () => {},
+    onError: (error) => {},
+  });
+
   return (
     <Box
       sx={{
@@ -24,12 +50,19 @@ function Note({ noteData = null }: NoteData) {
           <Box>something with a thought bubble</Box>
           <Box>
             <TextareaAutosize
-              value={noteData?.note ? noteData?.note : null}
+              value={note.note}
               placeholder="....."
               aria-label="write a note"
               minRows={15}
               style={{ width: 200 }}
+              onChange={(e) =>
+                setNote((prev) => ({ ...prev, note: e.target.value }))
+              }
             />
+          </Box>
+          <Box>
+            <Button onClick={() => setNote({ id: "", note: "" })}></Button>
+            <Button onClick={() => noteData}></Button>
           </Box>
         </Box>
       )}
