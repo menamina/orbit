60e85f94ae570b2b8a1dbf1148899c5d3833ac90
Mena <dayjacs03@gmail.com>
2026-09-06 13:38:44 -0500

    cycle comp + ts query

--- frontend/src/components/cycle/cycleTracking.tsx
+++ frontend/src/components/cycle/cycleTracking.tsx
@@ -5,6 +5,7 @@ import { useAuth } from "../../authContext";
 
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { getCycleByMonthYearQuery } from "../../tanstack/cycleTS";
+import { getNoteByDayQuery } from "../../tanstack/notesTS";
 
 import { ApiError } from "../../tanstack/api";
 import type { SettingsType } from "../tanstack/SettingsType";
@@ -16,14 +17,20 @@ import ErrorModal from "../popups/errorModal";
 import IconOptions from "./iconOptions";
 
 const today = new Date();
-const month = 
+const month = today.getMonth() + 1;
 const year = today.getFullYear();
 
-
-
 function CycleCalendar() {
+  const { accessToken, setAccessToken, setUser } = useAuth();
+  const queryClient = useQueryClient();
 
+  const { data: thisMonthsData, isPending: thisMonthPending } = useQuery({
+    ...getCycleByMonthYearQuery(month, year, accessToken, setAccessToken),
+  });
 
+  const { data: noteForToday, isPending: noteTodayPending } = useQuery({
+    ...getNoteByDayQuery(today, accessToken, setAccessToken),
+  });
 }
 
 export default CycleCalendar;
